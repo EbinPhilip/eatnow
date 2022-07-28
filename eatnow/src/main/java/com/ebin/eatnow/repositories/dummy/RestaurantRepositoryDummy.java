30ffb753eaa9766d73452f22cbb6a361e3d06c0a
Ebin <stargazer18@gmail.com>
2022-07-28 18:06:06 +0530

    Refactoring+serviceability check and selective items fetch

--- eatnow/src/main/java/com/ebin/eatnow/repositories/dummy/RestaurantRepositoryDummy.java
+++ eatnow/src/main/java/com/ebin/eatnow/repositories/dummy/RestaurantRepositoryDummy.java
@@ -16,43 +16,42 @@ import com.ebin.eatnow.utils.Location;
 public class RestaurantRepositoryDummy implements RestaurantRepository {
     private HashMap<String, Restaurant> restaurants;
 
-    public RestaurantRepositoryDummy()
-    {
+    public RestaurantRepositoryDummy() {
         restaurants = new HashMap<>();
 
         Restaurant r1 = Restaurant.builder()
-                            .id("r1")
-                            .name("r1")
-                            .address("")
-                            .location(new Location(11.1, 22.2))
-                            .tags(Arrays.asList("food", "yummy"))
-                            .rating(4.4)
-                            .reviews(10)
-                            .build();
+                .id("r1")
+                .name("r1")
+                .address("")
+                .location(new Location(11.1, 22.2))
+                .tags(Arrays.asList("food", "yummy"))
+                .rating(4.4)
+                .reviews(10)
+                .build();
         Restaurant r2 = Restaurant.builder()
-                            .id("r2")
-                            .name("r2")
-                            .address("")
-                            .location(new Location(11.1, 22.2))
-                            .rating(2.4)
-                            .reviews(50)
-                            .build();
+                .id("r2")
+                .name("r2")
+                .address("")
+                .location(new Location(11.1, 22.2))
+                .rating(2.4)
+                .reviews(50)
+                .build();
         Restaurant r3 = Restaurant.builder()
-                            .id("r3")
-                            .name("r3")
-                            .address("")
-                            .location(new Location(11.1, 22.2))
-                            .tags(Arrays.asList(""))
-                            .rating(3.4)
-                            .reviews(10)
-                            .build();
+                .id("r3")
+                .name("r3")
+                .address("")
+                .location(new Location(11.1, 22.2))
+                .tags(Arrays.asList(""))
+                .rating(3.4)
+                .reviews(10)
+                .build();
         Restaurant r4 = Restaurant.builder()
-                            .id("r4")
-                            .name("r4")
-                            .address("")
-                            .location(new Location(11.1, 22.2))
-                            .tags(Arrays.asList("yummy"))
-                            .build();
+                .id("r4")
+                .name("r4")
+                .address("")
+                .location(new Location(11.1, 22.2))
+                .tags(Arrays.asList("yummy"))
+                .build();
         restaurants.put(r1.getId(), r1);
         restaurants.put(r2.getId(), r2);
         restaurants.put(r3.getId(), r3);
@@ -60,37 +59,37 @@ public class RestaurantRepositoryDummy implements RestaurantRepository {
     }
 
     @Override
-    public Restaurant findById(String id)
-    {
+    public Restaurant findById(String id) {
         return restaurants.get(id);
     }
 
     @Override
-    public List<Restaurant> findById(List<String> id)
-    {
+    public List<Restaurant> findById(List<String> id) {
         return restaurants.values().stream()
-                .filter((i)->(id.contains(i.getId())))
+                .filter((i) -> (id.contains(i.getId())))
                 .collect(Collectors.toList());
     }
 
     @Override
-    public Map<String, Restaurant> findOpenRestaurantsNearby
-        (Location location, double distance)
-    {
+    public Map<String, Restaurant> findOpenRestaurantsNearby(Location location, double distance) {
         return restaurants.entrySet().stream()
-            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
     @Override
-    public boolean existsById(String id)
-    {
+    public boolean existsById(String id) {
         return restaurants.containsKey(id);
     }
 
     @Override
-    public Restaurant update(Restaurant restaurant)
-    {
+    public Restaurant update(Restaurant restaurant) {
         restaurants.put(restaurant.getId(), restaurant);
         return restaurant;
+    }
+
+    public boolean isRestaurantServiceable(String restaurantId,
+            Location location, double distance) {
+
+        return restaurants.containsKey(restaurantId);
     }
 }
