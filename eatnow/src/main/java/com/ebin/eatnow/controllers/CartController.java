fc207373346bad0cb2ac7c8907a54125a6518e96
Ebin <stargazer18@gmail.com>
2022-07-31 04:38:26 +0530

    Added security for CartController

--- eatnow/src/main/java/com/ebin/eatnow/controllers/CartController.java
+++ eatnow/src/main/java/com/ebin/eatnow/controllers/CartController.java
@@ -4,6 +4,7 @@ import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,55 +18,65 @@ import com.ebin.eatnow.services.CartService;
 
 @RestController
 public class CartController {
-    public static final String CART_ENDPOINT = "/cart";
-    public static final String CART_API = CART_ENDPOINT + "/{userId}";
+        public static final String CART_ENDPOINT = "/cart";
+        public static final String CART_API = CART_ENDPOINT + "/{userId}";
 
-    @Autowired
-    private CartService cartService;
+        @Autowired
+        private CartService cartService;
 
-    @GetMapping(CART_API)
-    public ResponseEntity<CartDto> getCart(
-            @PathVariable("userId") @NotNull String userId) {
+        @PreAuthorize("hasRole('ROLE_USER') and" +
+                        "#userId == authentication.principal.username")
+        @GetMapping(CART_API)
+        public ResponseEntity<CartDto> getCart(
+                        @PathVariable("userId") @NotNull String userId) {
 
-        return ResponseEntity.ok().body(
-                cartService.getCart(userId));
-    }
+                return ResponseEntity.ok().body(
+                                cartService.getCart(userId));
+        }
 
-    @PostMapping(CART_API)
-    public ResponseEntity<CartDto> postToCart(
-            @PathVariable("userId") @NotNull String userId,
-            @RequestParam("restaurantId") @NotNull String restaurantId,
-            @RequestParam("itemIndex") @NotNull Integer itemIndex,
-            @RequestParam("quantity") @NotNull Integer quantity) {
+        @PreAuthorize("hasRole('ROLE_USER') and" +
+                        "#userId == authentication.principal.username")
+        @PostMapping(CART_API)
+        public ResponseEntity<CartDto> postToCart(
+                        @PathVariable("userId") @NotNull String userId,
+                        @RequestParam("restaurantId") @NotNull String restaurantId,
+                        @RequestParam("itemIndex") @NotNull Integer itemIndex,
+                        @RequestParam("quantity") @NotNull Integer quantity) {
 
-        return ResponseEntity.ok().body(
-                cartService.addToCart(userId, restaurantId, itemIndex, quantity));
-    }
+                return ResponseEntity.ok().body(
+                                cartService.addToCart(userId, restaurantId, itemIndex, quantity));
+        }
 
-    @PutMapping(CART_API)
-    public ResponseEntity<CartDto> updateCart(
-            @PathVariable("userId") @NotNull String userId,
-            @RequestParam("itemIndex") @NotNull Integer itemIndex,
-            @RequestParam("quantity") @NotNull Integer quantity) {
+        @PreAuthorize("hasRole('ROLE_USER') and" +
+                        "#userId == authentication.principal.username")
+        @DeleteMapping(CART_API)
+        public ResponseEntity<Boolean> clearCart(
+                        @PathVariable("userId") @NotNull String userId) {
 
-        return ResponseEntity.ok().body(
-                cartService.updateCart(userId, itemIndex, quantity));
-    }
+                return ResponseEntity.ok().body(
+                                cartService.clearCart(userId));
+        }
 
-    @DeleteMapping(CART_API)
-    public ResponseEntity<Boolean> clearCart(
-            @PathVariable("userId") @NotNull String userId) {
+        @PreAuthorize("hasRole('ROLE_USER') and" +
+                        "#userId == authentication.principal.username")
+        @PutMapping(CART_API + "/{itemIndex}")
+        public ResponseEntity<CartDto> updateCart(
+                        @PathVariable("userId") @NotNull String userId,
+                        @PathVariable("itemIndex") @NotNull Integer itemIndex,
+                        @RequestParam("quantity") @NotNull Integer quantity) {
 
-        return ResponseEntity.ok().body(
-                cartService.clearCart(userId));
-    }
+                return ResponseEntity.ok().body(
+                                cartService.updateCart(userId, itemIndex, quantity));
+        }
 
-    @DeleteMapping(CART_API + "/{itemIndex}")
-    public ResponseEntity<CartDto> deleteFromCart(
-            @PathVariable("userId") @NotNull String userId,
-            @PathVariable("itemIndex") @NotNull Integer itemIndex) {
+        @PreAuthorize("hasRole('ROLE_USER') and" +
+                        "#userId == authentication.principal.username")
+        @DeleteMapping(CART_API + "/{itemIndex}")
+        public ResponseEntity<CartDto> deleteFromCart(
+                        @PathVariable("userId") @NotNull String userId,
+                        @PathVariable("itemIndex") @NotNull Integer itemIndex) {
 
-        return ResponseEntity.ok().body(
-                cartService.deleteFromCart(userId, itemIndex));
-    }
+                return ResponseEntity.ok().body(
+                                cartService.deleteFromCart(userId, itemIndex));
+        }
 }
