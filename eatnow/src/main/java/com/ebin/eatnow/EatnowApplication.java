b193c039906864febf0d6814f7d55c6430ef9c03
Ebin <stargazer18@gmail.com>
2022-08-01 02:50:42 +0530

    Added mongoDB repository and geospatial query

--- eatnow/src/main/java/com/ebin/eatnow/EatnowApplication.java
+++ eatnow/src/main/java/com/ebin/eatnow/EatnowApplication.java
@@ -2,8 +2,10 @@ package com.ebin.eatnow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class EatnowApplication {
 
 	public static void main(String[] args) {
