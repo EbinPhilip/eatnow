42f76040370d5dd2f66566daa7da369d107880e3
Ebin <stargazer18@gmail.com>
2022-08-01 23:40:49 +0530

    SecurityConfig: removed unused imports

--- eatnow/src/main/java/com/ebin/eatnow/config/SecurityConfig.java
+++ eatnow/src/main/java/com/ebin/eatnow/config/SecurityConfig.java
@@ -3,10 +3,7 @@ package com.ebin.eatnow.config;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
