e1b27f5aed397cfa7ee74b4aac343d8bf0119ce6
Ebin <stargazer18@gmail.com>
2022-08-04 19:47:05 +0530

    added redis cache to menu service

--- eatnow/src/main/java/com/ebin/eatnow/EatnowApplication.java
+++ eatnow/src/main/java/com/ebin/eatnow/EatnowApplication.java
@@ -2,10 +2,11 @@ package com.ebin.eatnow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
 @EnableMongoRepositories(basePackages = "com.ebin.eatnow.repositories.mongoDao")
 @EnableJpaRepositories(basePackages =  "com.ebin.eatnow.repositories.jpaDao")
 public class EatnowApplication {
--- eatnow/src/main/java/com/ebin/eatnow/repositories/impl/MenuRepositoryMongo.java
+++ eatnow/src/main/java/com/ebin/eatnow/repositories/impl/MenuRepositoryMongo.java
@@ -1,10 +1,16 @@
 package com.ebin.eatnow.repositories.impl;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +18,10 @@ import com.ebin.eatnow.entities.Item;
 import com.ebin.eatnow.entities.Menu;
 import com.ebin.eatnow.repositories.MenuRepository;
 import com.ebin.eatnow.repositories.mongoDao.MenuMongoDao;
+import com.ebin.eatnow.utils.RedisCache;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import redis.clients.jedis.Jedis;
 
 @Repository
 @Primary
@@ -20,24 +30,79 @@ public class MenuRepositoryMongo implements MenuRepository {
     @Autowired
     private MenuMongoDao dao;
 
+    private RedisCache cache;
+
+    @Value("${menu.redis.host}")
+    private String redisHost;
+
+    @Value("${menu.redis.port}")
+    private int redisPort;
+
+    @PostConstruct
+    private void initCache() {
+
+        cache = new RedisCache(redisHost, redisPort);
+    }
+
     public Menu findByRestaurantId(String id) {
 
-        return dao.findByRestaurantId(id)
+        return findByRestaurantIdFromCache(id);
+    }
+
+    private Menu findByRestaurantIdFromDb(String restaurantId) {
+
+        return dao.findByRestaurantId(restaurantId)
                 .orElseThrow(RuntimeException::new);
     }
 
-    private Menu findMenuByRestaurantIdAndItemIndex(String restaurantId, int itemIndex) {
+    private Menu findByRestaurantIdFromCache(String restaurantId) {
 
-        return dao.findByRetaurantIdAndItemIndex(restaurantId, itemIndex)
-        .orElseThrow(RuntimeException::new);
+        Menu menu = null;
+
+        try (Jedis jedis = cache.getResource()) {
+
+            String menuJson = jedis.get(restaurantId);
+            if (menuJson == null) {
+
+                menu = findByRestaurantIdFromDb(restaurantId);
+                menuJson = toJson(menu);
+                jedis.set(restaurantId, menuJson);
+            } else {
+
+                menu = fromJson(menuJson);
+            }
+        }
+
+        return menu;
+    }
+
+    private Menu fromJson(String menuJson) {
+
+        try {
+            return new ObjectMapper()
+                    .readValue(menuJson, Menu.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    private String toJson(Menu menu) {
+
+        try {
+            return new ObjectMapper()
+                    .writeValueAsString(menu);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
     }
 
     public Item findByRestaurantIdAndIndex(String restaurantId, int itemIndex) {
-        
-        Menu menu = findMenuByRestaurantIdAndItemIndex(restaurantId, itemIndex);
-        
+
+        Menu menu = findByRestaurantIdFromCache(restaurantId);
         return menu.getItems().stream()
-                .filter((i)->(i.getItemIndex() == itemIndex))
+                .filter((i) -> (i.getItemIndex() == itemIndex))
                 .findFirst()
                 .get();
     }
@@ -45,52 +110,92 @@ public class MenuRepositoryMongo implements MenuRepository {
     public List<Item> findByRestaurantIdAndIndices(String restaurantId,
             Set<Integer> indices) {
 
-        Menu menu = findByRestaurantId(restaurantId);
-
+        Menu menu = findByRestaurantIdFromCache(restaurantId);
         return menu.getItems()
                 .stream()
                 .filter(
-                    (i)->(indices.contains(i.getItemIndex()))
-                ).collect(Collectors.toList());
+                        (i) -> (indices.contains(i.getItemIndex())))
+                .collect(Collectors.toList());
     }
 
     public boolean existsByRestaurantId(String id) {
 
-        return dao.existsByRestaurantId(id);
+        try (Jedis jedis = cache.getResource()) {
+
+            if (jedis.get(id) != null) {
+                return true;
+            }
+        }
+        return dao.existsById(id);
+    }
+
+    private Menu saveMenu(Menu menu) {
+
+        try (Jedis jedis = cache.getResource()) {
+            jedis.set(menu.getRestaurantId(), toJson(menu));
+        }
+
+        return dao.save(menu);
     }
 
     public Item createItem(String restaurantId, Item item) {
 
-        Menu menu = findByRestaurantId(restaurantId);
-        int index = (int) menu.getItems().stream().count() + 1;
+        Menu menu = findByRestaurantIdFromCache(restaurantId);
+
+        int index = Collections.max(menu.getItems(), new Comparator<Item>() {
+            @Override
+            public int compare(Item i1, Item i2) {
+                return Integer.valueOf(i1.getItemIndex())
+                        .compareTo(i2.getItemIndex());
+            }
+        }).getItemIndex() + 1;
+
         item.setItemIndex(index);
         menu.getItems().add(item);
 
-        dao.save(menu);
+        saveMenu(menu);
 
         return item;
     }
 
+    private Integer findListPositionOfItem(Menu menu, int itemIndex) {
+
+        int index = 0;
+        for (Item elem : menu.getItems()) {
+
+            if (elem.getItemIndex() == itemIndex) {
+
+                return index;
+            }
+            ++index;
+        }
+
+        return null;
+    }
+
     public Item updateItem(String restaurantId, int itemIndex, Item item) {
 
-        Menu menu = findMenuByRestaurantIdAndItemIndex(restaurantId, itemIndex);
+        Menu menu = findByRestaurantIdFromCache(restaurantId);
+        int index = Optional.ofNullable(findListPositionOfItem(menu, itemIndex))
+                .orElseThrow(RuntimeException::new);
 
-        menu.getItems().removeIf((i)->(i.getItemIndex() == itemIndex));
         item.setItemIndex(itemIndex);
-        menu.getItems().add(item);
+        menu.getItems().set(index, item);
 
-        dao.save(menu);
+        saveMenu(menu);
 
         return item;
     }
 
     public boolean deleteItem(String restaurantId, int itemIndex) {
 
-        Menu menu = findMenuByRestaurantIdAndItemIndex(restaurantId, itemIndex);
+        Menu menu = findByRestaurantIdFromCache(restaurantId);
+        int index = Optional.ofNullable(findListPositionOfItem(menu, itemIndex))
+                .orElseThrow(RuntimeException::new);
 
-        menu.getItems().removeIf((i)->(i.getItemIndex() == itemIndex));
+        menu.getItems().remove(index);
 
-        dao.save(menu);
+        saveMenu(menu);
 
         return true;
     }
