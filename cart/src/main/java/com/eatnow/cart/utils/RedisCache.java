a97935d177daeb5c5208f09736f5a2b667be7a48
Ebin <stargazer18@gmail.com>
2022-08-05 22:25:39 +0530

    cart: Added utilities

---
+++ cart/src/main/java/com/eatnow/cart/utils/RedisCache.java
@@ -0,0 +1,35 @@
+package com.eatnow.cart.utils;
+
+import java.time.Duration;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class RedisCache {
+
+    private JedisPool jedisPool;
+
+    public RedisCache(String host, int port) {
+
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(128);
+        poolConfig.setMaxIdle(128);
+        poolConfig.setMinIdle(16);
+        poolConfig.setTestOnBorrow(true);
+        poolConfig.setTestOnReturn(true);
+        poolConfig.setTestWhileIdle(true);
+        poolConfig.setMinEvictableIdleTime(Duration.ofSeconds(60));
+        poolConfig.setTimeBetweenEvictionRuns(Duration.ofSeconds(30));
+        poolConfig.setNumTestsPerEvictionRun(3);
+        poolConfig.setBlockWhenExhausted(true);
+
+        jedisPool = new JedisPool(poolConfig, host, port);
+    }
+
+    public Jedis getResource() {
+
+        return jedisPool.getResource();
+    }
+
+}
