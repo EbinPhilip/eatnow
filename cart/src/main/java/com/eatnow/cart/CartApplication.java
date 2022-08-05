a0d59255c97d08c464bd48ae26c019de1534e31e
Ebin <stargazer18@gmail.com>
2022-08-06 02:16:27 +0530

    Cart: Added menu service with feign client

--- cart/src/main/java/com/eatnow/cart/CartApplication.java
+++ cart/src/main/java/com/eatnow/cart/CartApplication.java
@@ -2,12 +2,15 @@ package com.eatnow.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableFeignClients
+@EnableMongoRepositories(basePackages = "com.eatnow.cart.repositories.mongoDao")
 @SpringBootApplication
 public class CartApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CartApplication.class, args);
 	}
-
 }
---
+++ cart/src/main/java/com/eatnow/cart/services/clients/MenuRestClient.java
@@ -0,0 +1,15 @@
+package com.eatnow.cart.services.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.eatnow.cart.dtos.MenuDto;
+
+@FeignClient(value = "menu-client", url = "localhost:8081")
+public interface MenuRestClient {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/menu/{restaurantId}")
+    MenuDto getMenu(@PathVariable("restaurantId") String restaurantId);
+}
