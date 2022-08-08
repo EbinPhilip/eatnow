7e7f33257859b7b1d4c22bd0f023f5b93c172722
Ebin <stargazer18@gmail.com>
2022-08-08 19:25:44 +0530

    Order: dockerized

--- order/src/main/java/com/eatnow/order/OrderApplication.java
+++ order/src/main/java/com/eatnow/order/OrderApplication.java
@@ -2,10 +2,11 @@ package com.eatnow.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
 @EnableJpaRepositories(basePackages =  "com.eatnow.order.repositories.jpaDao")
 @EnableFeignClients
 public class OrderApplication {
