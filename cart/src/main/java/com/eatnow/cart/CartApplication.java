9d5632df950848dcb7b74cbdc877a7894d58245b
Ebin <stargazer18@gmail.com>
2022-08-08 19:25:27 +0530

    Cart: dockerized

--- cart/src/main/java/com/eatnow/cart/CartApplication.java
+++ cart/src/main/java/com/eatnow/cart/CartApplication.java
@@ -2,12 +2,13 @@ package com.eatnow.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableFeignClients
 @EnableMongoRepositories(basePackages = "com.eatnow.cart.repositories.mongoDao")
-@SpringBootApplication
+@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
 public class CartApplication {
 
 	public static void main(String[] args) {
