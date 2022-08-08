71b9a248b0bbff8ffdacf9679696c64b2682dcb0
Ebin <stargazer18@gmail.com>
2022-08-08 19:26:23 +0530

    User: dockerized

--- user/src/main/java/com/eatnow/user/UserApplication.java
+++ user/src/main/java/com/eatnow/user/UserApplication.java
@@ -2,9 +2,10 @@ package com.eatnow.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
 @EnableJpaRepositories(basePackages =  "com.eatnow.user.repositories.jpaDao")
 public class UserApplication {
 
