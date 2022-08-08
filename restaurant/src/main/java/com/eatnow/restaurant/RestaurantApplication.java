6e38d506d7e6e3f4991a8ca76a12b191fb4c95b5
Ebin <stargazer18@gmail.com>
2022-08-08 19:26:04 +0530

    Restaurant: dockerized

--- restaurant/src/main/java/com/eatnow/restaurant/RestaurantApplication.java
+++ restaurant/src/main/java/com/eatnow/restaurant/RestaurantApplication.java
@@ -2,9 +2,10 @@ package com.eatnow.restaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
 @EnableMongoRepositories(basePackages = "com.eatnow.restaurant.repositories.mongoDao")
 public class RestaurantApplication {
 
