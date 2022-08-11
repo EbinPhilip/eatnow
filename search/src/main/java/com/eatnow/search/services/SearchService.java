83744e2a153eb1a89b58aca979dd9995c142bb22
Ebin <stargazer18@gmail.com>
2022-08-11 18:02:54 +0530

    Search: supports page numbers and user addresses

--- search/src/main/java/com/eatnow/search/services/SearchService.java
+++ search/src/main/java/com/eatnow/search/services/SearchService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import com.eatnow.search.dtos.ItemDto;
 import com.eatnow.search.dtos.RestaurantDto;
+import com.eatnow.search.dtos.UserAddressDto;
 import com.eatnow.search.entities.Item;
 import com.eatnow.search.entities.Restaurant;
 import com.eatnow.search.repositories.ItemRepository;
@@ -26,7 +27,10 @@ public class SearchService {
     @Autowired
     private ItemRepository itemRepository;
 
-    public Page<RestaurantDto> searchRestaurants(String query,
+    @Autowired
+    private UserAddressService userAddressService;
+
+    public Page<RestaurantDto> searchRestaurantsNearby(String query,
             double latitude, double longitude, int page, int size) {
 
         Pageable pageable = PageRequest.of(page, size);
@@ -45,7 +49,16 @@ public class SearchService {
                 });
     }
 
-    public Page<ItemDto> searchItems(String query,
+    public Page<RestaurantDto> searchRestaurantsNearUserAddress(String query,
+            String userId, int addressIndex, int page, int size) {
+
+        UserAddressDto address = userAddressService
+                        .getAddressByUserIdAndIndex(userId, addressIndex);
+        return searchRestaurantsNearby(query, address.getLatitude(),
+                        address.getLongitude(), page, size);
+    }
+
+    public Page<ItemDto> searchItemsNearby(String query,
             double latitude, double longitude, int page, int size) {
 
         Pageable pageable = PageRequest.of(page, size);
@@ -64,5 +77,14 @@ public class SearchService {
                             .tags(Arrays.asList(i.getTags().split(" ")))
                             .build();
                 });
+    }
+
+    public Page<ItemDto> searchItemsNearUserAddress(String query,
+            String userId, int addressIndex, int page, int size) {
+
+        UserAddressDto address = userAddressService
+                .getAddressByUserIdAndIndex(userId, addressIndex);
+        return searchItemsNearby(query, address.getLatitude(),
+                address.getLongitude(), page, size);
     }
 }
