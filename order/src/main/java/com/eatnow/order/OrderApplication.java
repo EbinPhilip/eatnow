923768be724bdfd3a298e82285c53909fe74075b
Ebin <stargazer18@gmail.com>
2022-08-14 22:57:15 +0530

    Order: Added swagger

--- order/src/main/java/com/eatnow/order/OrderApplication.java
+++ order/src/main/java/com/eatnow/order/OrderApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServic
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
 @EnableJpaRepositories(basePackages =  "com.eatnow.order.repositories.jpaDao")
+@OpenAPIDefinition(info = @Info(title = "Order", version = "1.0", description = "APIs for order management"))
 @EnableFeignClients
 public class OrderApplication {
 
