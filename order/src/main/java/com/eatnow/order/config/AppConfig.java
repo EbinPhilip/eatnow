7a25b5dd24d349216d17b391f82998ec9bfaaaaa
Ebin <stargazer18@gmail.com>
2022-08-15 02:09:04 +0530

    Order: added feign error handling

--- order/src/main/java/com/eatnow/order/config/AppConfig.java
+++ order/src/main/java/com/eatnow/order/config/AppConfig.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.eatnow.order.utils.FeignErrorDecoder;
 import com.eatnow.order.utils.RedisCache;
+
+import feign.codec.ErrorDecoder;
 
 @Configuration
 public class AppConfig {
@@ -19,5 +22,11 @@ public class AppConfig {
 	public RedisCache getRedisCache() {
 
 		return new RedisCache(redisHost, redisPort);
-	} 
+	}
+
+    @Bean
+    public ErrorDecoder errorDecoder() {
+        return new FeignErrorDecoder();
+    }
+
 }
