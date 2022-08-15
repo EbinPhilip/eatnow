e992860c6a0fbdd94616f911e954dde6069837d0
Ebin <stargazer18@gmail.com>
2022-08-16 00:45:17 +0530

    Order: added repository paged find and sort by timestamp

--- order/src/main/java/com/eatnow/order/repositories/jpaDao/OrderJpaDao.java
+++ order/src/main/java/com/eatnow/order/repositories/jpaDao/OrderJpaDao.java
@@ -4,9 +4,9 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.eatnow.order.entities.OrderEntity;
 
@@ -14,15 +14,9 @@ public interface OrderJpaDao extends JpaRepository<OrderEntity, UUID> {
     
     public List<OrderEntity> findByUserIdAndTimeStampGreaterThan(String userId, LocalDateTime timestamp);
 
-    @Query(value = "SELECT * FROM \"order\" WHERE USER_ID = ?1",
-        countQuery = "SELECT count(*) FROM \"order\" WHERE USER_ID = ?1",
-        nativeQuery = true)
-    public List<OrderEntity> findByUserId(String userId, Pageable page);
+    public Page<OrderEntity> findByUserIdOrderByTimeStampDesc(String userId, Pageable page);
 
     public List<OrderEntity> findByRestaurantIdAndTimeStampGreaterThan(String restaurantId, LocalDateTime timestamp);
 
-    @Query(value = "SELECT * FROM \"order\" WHERE RESTAURANT_ID = ?1",
-        countQuery = "SELECT count(*) FROM \"order\" WHERE RESTAURANT_ID = ?1",
-        nativeQuery = true)
-    public List<OrderEntity> findByRestaurantId(String restaurantId, Pageable page);
+    public Page<OrderEntity> findByRestaurantIdOrderByTimeStampDesc(String restaurantId, Pageable page);
 }
